00044a14b5835a6ac08f678572e2872c24bb5822
Anthony Gomez <gomez9162@gmail.com>
2023-01-04 09:01:09 -0600

    Refactored App
    Refactored App

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,6 +8,41 @@ const Header = ({ course }) => {
   );
 };
 
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundementals of React';
@@ -20,16 +55,19 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </>
   );
 };
