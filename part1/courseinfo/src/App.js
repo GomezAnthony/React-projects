e0a0bf3e14435bfc910404a65491033e64e4a72c
Anthony Gomez <gomez9162@gmail.com>
2023-01-12 09:53:28 -0600

    Removed Comments
    Remove Comments

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -44,26 +44,8 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 };
 
 const App = () => {
-  // const course = 'Half Stack application development';
-  // const part1 = 'Fundementals of React';
-  // const exercises1 = 10;
-  // const part2 = 'Using props to pass data';
-  // const exercises2 = 7;
-  // const part3 = 'State of a component';
-  // const exercises3 = 14;
   const course = 'Half Stack application development';
-  // const part1 = {
-  //   name: 'Fundementals of React',
-  //   exercises: 10,
-  // };
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7,
-  // };
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14,
-  // };
+
   const parts = [
     {
       name: 'Fundementals of React',
