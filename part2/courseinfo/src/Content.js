35647c03dcf791cc5aefb062810e1f9225d91c66
Anthony Gomez <gomez9162@gmail.com>
2023-01-25 10:18:16 -0600

    courseInfo
    CourseInfo FSO

--- part2/courseinfo/src/Content.js
+++ part2/courseinfo/src/Content.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Part from './Part';
 
 function Content({ course }) {
+  const total = course.parts.reduce((acc, curr) => {
+    return acc + curr.exercises;
+  }, 0);
+
+  console.log(total);
   return (
     <div>
       {course.parts.map((part) => (
-        <Part part={part} />
+        <Part key={part.id} part={part} />
       ))}
+      <p>Total of {total} exercises</p>
     </div>
   );
 }
