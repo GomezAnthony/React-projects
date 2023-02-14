4b6691912febddc3c96098b7721ef188e6f64ced
Anthony Gomez <gomez9162@gmail.com>
2023-02-14 10:18:53 -0600

    Country filter function
    added the filter function based on input

---
+++ part2/countries/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  // Create a hook to handle state change based on the input
+  const [countries, setCountries] = useState([]);
+  const [filtered, setFiltred] = useState('');
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
+  // Handle function will allow to handle the chnage and update the state, based on the user input
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setFiltred(e.target.value);
+  };
+
+  // If the nothing is being inputed by the user, than display the whole list of countries.
+  // If something is being typed into the input, filter the data based on the name inside the
+  // country data set, with the input query and display the correct result.
+  const filteredData =
+    filtered === ''
+      ? countries
+      : countries.filter((data) =>
+          data.name.common.toLowerCase().includes(filtered.toLowerCase())
+        );
+  return (
+    <div>
+      <p>
+        Search for countries: <input onChange={handleChange} value={filtered} />
+      </p>
+      {filteredData.map((count) => (
+        <li key={count.id}>{count.name.common}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
