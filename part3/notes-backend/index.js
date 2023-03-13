2fc381acd4c2764125d7e018ad065b93fbdcf70a
Anthony Gomez <gomez9162@gmail.com>
2023-03-12 22:20:55 -0500

    Noyes backend
    fullstackopen backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,5 +1,9 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 let notes = [
   {
@@ -25,6 +29,50 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+  response.json(note);
+});
+
+app.delete('api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 const PORT = 3001;
